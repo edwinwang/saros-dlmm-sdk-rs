ab45cfa1720fb57522cca7e399225714fffa2660
edwin <>
2025-10-27 22:41:57 +0800

    update

--- rewarder-hook/src/lib.rs
+++ rewarder-hook/src/lib.rs
@@ -1,1 +1,13 @@
+#[cfg(not(feature = "no-entrypoint"))]
 anchor_gen::generate_cpi_crate!("idls/rewarder_hook.json");
+
+#[cfg(feature = "no-entrypoint")]
+pub mod prelude {
+    use solana_sdk::pubkey;
+    use solana_sdk::pubkey::Pubkey;
+
+    pub const ID: Pubkey = pubkey!("mdmavMvJpF4ZcLJNg6VSjuKVMiBo5uKwERTg1ZB9yUH");
+}
+
+#[cfg(feature = "no-entrypoint")]
+pub use prelude::*;
